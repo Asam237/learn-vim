4aaf0db2036333e478a3b65b280db35b66d0f2f2
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-20 11:49:55 +0100

    use macros

--- src/macros.sql
+++ src/macros.sql
@@ -1,35 +1,35 @@
-Aamir Abagael
-Aaron Abagail
-Abbey Abbe
-Abbie Abbey
-Abbot Abbi
-Abbott Abbie
-Abby Abby
-Abdel Abigael
-Abdul Abigail
-Abdulkarim Abigale
-Abdullah Abra
-Abe Acacia
-Abel Ada
-Abelard Adah
-Abner Adaline
-Abraham Adara
-Abram Addie
-Ace Addis
-Adair Adel
-Adam Adela
-Adams Adelaide
-Addie Adele
-Adger Adelice
-Aditya Adelina
-Adlai Adelind
-Adnan Adeline
-Adolf Adella
-Adolfo Adelle
-Adolph Adena
-Adolphe Adey
-Adolpho Adi
-Adolphus Adiana
-Adrian Adina
-Adrick Adora
-Adrien Adore
+INSERT INTO(firstName, lastName) VALUES("Aamir", "Abagael");
+INSERT INTO(firstName, lastName) VALUES("Aaron", "Abagail");
+INSERT INTO(firstName, lastName) VALUES("Abbey", "Abbe");
+INSERT INTO(firstName, lastName) VALUES("Abbie", "Abbey");
+INSERT INTO(firstName, lastName) VALUES("Abbot", "Abbi");
+INSERT INTO(firstName, lastName) VALUES("Abbott", "Abbie");
+INSERT INTO(firstName, lastName) VALUES("Abby", "Abby");
+INSERT INTO(firstName, lastName) VALUES("Abdel", "Abigael");
+INSERT INTO(firstName, lastName) VALUES("Abdul", "Abigail");
+INSERT INTO(firstName, lastName) VALUES("Abdulkarim", "Abigale");
+INSERT INTO(firstName, lastName) VALUES("Abdullah", "Abra");
+INSERT INTO(firstName, lastName) VALUES("Abe", "Acacia");
+INSERT INTO(firstName, lastName) VALUES("Abel", "Ada");
+INSERT INTO(firstName, lastName) VALUES("Abelard", "Adah");
+INSERT INTO(firstName, lastName) VALUES("Abner", "Adaline");
+INSERT INTO(firstName, lastName) VALUES("Abraham", "Adara");
+INSERT INTO(firstName, lastName) VALUES("Abram", "Addie");
+INSERT INTO(firstName, lastName) VALUES("Ace", "Addis");
+INSERT INTO(firstName, lastName) VALUES("Adair", "Adel");
+INSERT INTO(firstName, lastName) VALUES("Adam", "Adela");
+INSERT INTO(firstName, lastName) VALUES("Adams", "Adelaide");
+INSERT INTO(firstName, lastName) VALUES("Addie", "Adele");
+INSERT INTO(firstName, lastName) VALUES("Adger", "Adelice");
+INSERT INTO(firstName, lastName) VALUES("Aditya", "Adelina");
+INSERT INTO(firstName, lastName) VALUES("Adlai", "Adelind");
+INSERT INTO(firstName, lastName) VALUES("Adnan", "Adeline");
+INSERT INTO(firstName, lastName) VALUES("Adolf", "Adella");
+INSERT INTO(firstName, lastName) VALUES("Adolfo", "Adelle");
+INSERT INTO(firstName, lastName) VALUES("Adolph", "Adena");
+INSERT INTO(firstName, lastName) VALUES("Adolphe", "Adey");
+INSERT INTO(firstName, lastName) VALUES("Adolpho", "Adi");
+INSERT INTO(firstName, lastName) VALUES("Adolphus", "Adiana");
+INSERT INTO(firstName, lastName) VALUES("Adrian", "Adina");
+INSERT INTO(firstName, lastName) VALUES("Adrick", "Adora");
+INSERT INTO(firstName, lastName) VALUES("Adrien", "Adore");
